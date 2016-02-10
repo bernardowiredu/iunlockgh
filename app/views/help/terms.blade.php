05337731bf522dfe13d4836e1fd9eafa5efcd0d7
bernardowiredu <bernardkissi18@gmail.com>
2016-02-10 14:03:35 +0000

    Terms and Condition page has been fixed
    i just added new terms and condition page

---
+++ app/views/help/terms.blade.php
@@ -0,0 +1,16 @@
+@extends('layout.holder')
+<br><br>
+
+
+
+@section('content')
+
+<br><br>
+<div class="col-sm-offset-2 col-md-8 mf">
+<h1 class="blck2">Terms and Conditions</h1>
+<p>You can make payment into your Perfectunlock Account for the purchase of any unlocking service and if paying directly without your account for any order make sure you make Mobile Transfer Payment before making online payment for your order.In making mobile transfer use your account Username as Reference ID.</p>
+
+
+</div>
+
+@stop
