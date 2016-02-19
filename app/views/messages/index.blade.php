f628590986685b5517172efda4df711ce56869d0
bernardowiredu <bernardkissi18@gmail.com>
2016-02-19 16:03:46 +0000

    user message sender function has been fixed
    user message function has been fixed but yet to work (interphase build)

--- app/views/messages/index.blade.php
+++ app/views/messages/index.blade.php
@@ -85,6 +85,23 @@
 
         </p>
     </div>
+    <div class="tab-pane active" id="tab2">
+    <form>
+      <div class="form-group">
+        <label for="exampleInputEmail1" class="blck2">Email address</label>
+        <input type="email" class="form-control" id="exampleInputEmail1" placeholder="Email">
+      </div>
+      <div class="form-group">
+        <label for="exampleInputPassword1" class="blck2">Password</label>
+        <input type="password" class="form-control" class="blck2" id="exampleInputPassword1" placeholder="Password">
+      </div>
+      <div class="form-group">
+        <label for="exampleInputFile" class="blck2">Send message</label>
+        <textarea class="form-control" cols="10" rows="10"></textarea>
+      </div>
+      <button type="submit" class="btn btn-primary">Submit</button>
+    </form>
     </div>
     </div>
-@stop
+    </div>
+@stop 
